44d52fe86296b6697093361c976122ff591cb4f5
jatin sharma <01jatinsharma01@gmail.com>
2019-02-07 03:32:41 +0000

    Update Restapi.js

--- Frontend/src/services/Restapi.js
+++ Frontend/src/services/Restapi.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+ import Vue from 'vue'
 export default {
   
   PricesData (callback) {
@@ -10,16 +10,220 @@ export default {
           'Content-Type': 'application/json'
         }
       })
-
         .then(function (response) {
          console.log(response)
           callback (response)
         })
         .catch((error) => {
           console.log(error)
-          // reject(error)
         })
-    //})
   },
-  
+  RebarData(){
+    return new Promise((resolve, reject) => {
+    Vue.axios({
+    method: 'POST',
+    url: 'http://137.116.113.220:7002',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data:{
+      'query': 'rebar'
+      }
+    })
+    .then(function (response) {
+    console.log(response)
+    resolve(response)
+    })
+    .catch((error) => {
+    console.log(error)
+    reject(error)
+    })
+    }) 
+    },
+
+    commonData(){
+      return new Promise((resolve, reject) => {
+      Vue.axios({
+      method: 'POST',
+      url: 'http://137.116.113.220:7002',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data:{
+        'query': 'common carbon round steel'
+        }
+      })
+      .then(function (response) {
+      console.log(response)
+      resolve(response)
+      })
+      .catch((error) => {
+      console.log(error)
+      reject(error)
+      })
+      }) 
+      },
+
+      carbonData(){
+        return new Promise((resolve, reject) => {
+        Vue.axios({
+        method: 'POST',
+        url: 'http://137.116.113.220:7002',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data:{
+          'query': 'carbon structural steel'
+          }
+        })
+        .then(function (response) {
+        console.log(response)
+        resolve(response)
+        })
+        .catch((error) => {
+        console.log(error)
+        reject(error)
+        })
+        }) 
+        },
+
+        wireData(){
+          return new Promise((resolve, reject) => {
+          Vue.axios({
+          method: 'POST',
+          url: 'http://137.116.113.220:7002',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data:{
+            'query': 'wire rod'
+            }
+          })
+          .then(function (response) {
+          console.log(response)
+          resolve(response)
+          })
+          .catch((error) => {
+          console.log(error)
+          reject(error)
+          })
+          }) 
+          },
+
+          RoundData(){
+            return new Promise((resolve, reject) => {
+            Vue.axios({
+            method: 'POST',
+            url: 'http://137.116.113.220:7002',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            data:{
+              'query': 'round bearing steel'
+              }
+            })
+            .then(function (response) {
+            console.log(response)
+            resolve(response)
+            })
+            .catch((error) => {
+            console.log(error)
+            reject(error)
+            })
+            }) 
+            },
+
+              coldData(){
+              return new Promise((resolve, reject) => {
+              Vue.axios({
+              method: 'POST',
+              url: 'http://137.116.113.220:7002',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              data:{
+                'query': 'cold steel'
+                }
+              })
+              .then(function (response) {
+              console.log(response)
+              resolve(response)
+              })
+              .catch((error) => {
+              console.log(error)
+              reject(error)
+              })
+              }) 
+              },
+
+              colorData(){
+                return new Promise((resolve, reject) => {
+                Vue.axios({
+                method: 'POST',
+                url: 'http://137.116.113.220:7002',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                data:{
+                  'query': 'color-coated steel'
+                  }
+                })
+                .then(function (response) {
+                console.log(response)
+                resolve(response)
+                })
+                .catch((error) => {
+                console.log(error)
+                reject(error)
+                })
+                }) 
+                },
+
+                galvanishedData(){
+                  return new Promise((resolve, reject) => {
+                  Vue.axios({
+                  method: 'POST',
+                  url: 'http://137.116.113.220:7002',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  data:{
+                    'query': 'galvanished'
+                    }
+                  })
+                  .then(function (response) {
+                  console.log(response)
+                  resolve(response)
+                  })
+                  .catch((error) => {
+                  console.log(error)
+                  reject(error)
+                  })
+                  }) 
+                  },
+
+                  HRCData(){
+                    return new Promise((resolve, reject) => {
+                    Vue.axios({
+                    method: 'POST',
+                    url: 'http://137.116.113.220:7002',
+                    headers: {
+                      'Content-Type': 'application/json'
+                    },
+                    data:{
+                      'query': 'HRC'
+                      }
+                    })
+                    .then(function (response) {
+                    console.log(response)
+                    resolve(response)
+                    })
+                    .catch((error) => {
+                    console.log(error)
+                    reject(error)
+                    })
+                    }) 
+                    },
 }
+
+
